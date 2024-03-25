ebefd3933e0cc9eef6afe902d69c441f2cab5798
zhangweisong02 <zhangweisong02@meituan.com>
2024-03-25 17:46:37 +0800

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "History QR Code – create QR codes for free",
-  description: "History QR Code – create QR codes for free，save the previously generated QR code",
+  description:
+    "History QR Code – create QR codes for free，save the previously generated QR code",
 };
 
 export default function RootLayout({
@@ -17,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><AntdRegistry>{children}</AntdRegistry></body>
+      <body className={inter.className}>
+        <AntdRegistry>{children}</AntdRegistry>
+        <Analytics />
+      </body>
     </html>
   );
 }
