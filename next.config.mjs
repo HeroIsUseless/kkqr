316a1dd23762b2e29e532787ba6fa7d8c52d26cd
zhangweisong <zhangweisong.zws@alibaba-inc.com>
2025-11-06 14:12:14 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
 };
 
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: false, // 启用 PWA(包括开发模式)
-})(nextConfig);
+export default nextConfig;
